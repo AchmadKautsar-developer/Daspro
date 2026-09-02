00f1b8ef43b3303946af5d1f054a04775d7884e8
AchmadKautsar-developer <achkadeveloper@gmail.com>
2026-09-01 21:50:52 -0500

    Delete Tugas Daspro/Praktek Daspro directory

--- Tugas Daspro/Praktek Daspro/MyFirstJava01_01.java
+++
@@ -1,9 +0,0 @@
-public class MyFirstJava01_01 {
-     public static void main(String[] args) throws Exception {
-
-        System.out.println("Hello, My name is Achmad kautsar Ali Mu'minin");
-        System.out.println("Belajar Java itu muda");
-        System.out.println("Semakin Sering Mencoba, Seemakin Jago");
-
-    }
-}
